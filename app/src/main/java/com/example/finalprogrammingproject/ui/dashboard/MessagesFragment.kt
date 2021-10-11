66f0259090a11861c6567195b7be794f3d1f8cdd
Sean Morrissey <semorrissey17@gmail.com>
2021-10-10 21:00:19 -0400

    setup fragments with navigation

---
+++ app/src/main/java/com/example/finalprogrammingproject/ui/dashboard/MessagesFragment.kt
@@ -0,0 +1,45 @@
+package com.example.finalprogrammingproject.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.finalprogrammingproject.R
+import com.example.finalprogrammingproject.databinding.FragmentMessagesBinding
+
+class MessagesFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: MessagesViewModel
+    private var _binding: FragmentMessagesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(MessagesViewModel::class.java)
+
+        _binding = FragmentMessagesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textMessages
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/finalprogrammingproject/ui/notifications/SettingsFragment.kt
@@ -0,0 +1,45 @@
+package com.example.finalprogrammingproject.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.finalprogrammingproject.R
+import com.example.finalprogrammingproject.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: SettingsViewModel
+    private var _binding: FragmentSettingsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSettings
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
