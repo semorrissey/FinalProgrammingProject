c4abaf0983e2f4f62ba18d3157d9bd472fd643f7
syrenetidelacruz <syrenetidelacruz4@gmail.com>
2021-10-12 16:07:10 -0400

    xml and blocked

--- app/src/main/java/com/example/finalprogrammingproject/ui/PopupBlockedActivity.kt
+++ app/src/main/java/com/example/finalprogrammingproject/ui/PopupBlockedActivity.kt
@@ -1,4 +1,50 @@
 package com.example.finalprogrammingproject.ui
 
-class PopupBlockedActivity {
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.LayoutInflater.from
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.app.NotificationManagerCompat.from
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalprogrammingproject.R
+import com.example.finalprogrammingproject.databinding.FragmentMessagesBinding
+import com.example.finalprogrammingproject.ui.dashboard.MessagesViewModel
+
+class PopupBlockedActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.blocked_popup)
+
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        var width = displayMetrics.widthPixels
+        var height = displayMetrics.heightPixels
+
+        var displayWidth = (width * .8).toInt()
+        var displayHeight = (width * .6).toInt()
+
+
+        window.setLayout(displayWidth, displayHeight)
+
+        var actionBar = getSupportActionBar()
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
 }
